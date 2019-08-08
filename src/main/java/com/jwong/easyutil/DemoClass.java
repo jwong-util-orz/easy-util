d5b96047f844523622b8f16a8107216c934b20b5
wang_jiping <imwangjiping@sina.com>
2019-08-08 10:48:48 +0800

    init project

---
+++ src/main/java/com/jwong/easyutil/DemoClass.java
@@ -0,0 +1,14 @@
+package com.jwong.easyutil;
+
+/**
+ * DemoClass class
+ *
+ * @author J.Wong
+ * @date 2019/08/08
+ */
+public class DemoClass {
+
+    public static String toUpperCase(){
+        return "";
+    }
+}
